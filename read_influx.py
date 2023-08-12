2fa4e32c0bbb6f7ff5754b31011e4744117807e7
Tecfer <carnazatui@gmail.com>
2023-08-12 10:21:38 +0200

    fix requirements

--- read_influx.py
+++ read_influx.py
@@ -1,34 +1,6 @@
-#pip install requests
-import requests
-#pip install influxdb
-import influxdb
-
-'''
-Ejemplo de try import
-try:
-    import requests
-except ImportError:
-    print("El módulo 'requests' no está instalado.")
-    install_requests = input("¿Deseas instalarlo ahora? (s/n): ")
-    if install_requests.lower() == 's':
-        try:
-            import subprocess
-            subprocess.check_call(['pip', 'install', 'requests'])
-            print("El módulo 'requests' se ha instalado correctamente.")
-        except Exception as e:
-            print("Ha ocurrido un error al intentar instalar 'requests':", e)
-    else:
-        print("No se ha instalado el módulo 'requests'.")
-else:
-    print("El módulo 'requests' está instalado.")
-
-'''
-
-'''
-
-Ejemplo de try requirements
 import subprocess
 import sys
+import pkg_resources
 
 def install_requirements(requirements_file):
     try:
@@ -37,15 +9,19 @@ def install_requirements(requirements_file):
         print("Error al instalar las dependencias:", e)
         sys.exit(1)
 
-def main():
+def requirements_check():
     requirements_file = "requirements.txt"
     try:
         with open(requirements_file, "r") as f:
             required_packages = [line.strip() for line in f.readlines()]
         
-        installed_packages = [pkg.key for pkg in pkg_resources.working_set]
+        installed_packages = [pkg.key for pkg in pkg_resources.working_set]     
         missing_packages = [pkg for pkg in required_packages if pkg not in installed_packages]
-
+        print("dev: Installed pakages")
+        print(installed_packages)
+        print("dev: Missed pakages")
+        print(missing_packages)
+        print("dev...")
         if missing_packages:
             print("Faltan dependencias:")
             for pkg in missing_packages:
@@ -56,69 +32,72 @@ def main():
         else:
             print("Todas las dependencias están instaladas.")
         
-        # Aquí puedes ejecutar tu archivo Python
-        # por ejemplo:
-        # import tu_archivo
+        
     except FileNotFoundError:
         print(f"No se encontró el archivo {requirements_file}.")
 
-if __name__ == "__main__":
-    main()
 
-'''
-
-#pip install python-dotenv
-from dotenv import load_dotenv
-
-def connet_bbdd(key):
+def connet_bbdd(server_ip,server_port,key):
     print(f"ha seleccionado {key}")
+    import influxdb
+    try:
+        client = influxdb.InfluxDBClient(host=server_ip, port=server_port, database=key)
+        print("Conexión exitosa.")
+        
+        result = client.query("SHOW MEASUREMENTS;")
+        print("---- SHOW MEASUREMENTS ----")
+        lista = list(result.get_points())
+        
+        for idx, item in enumerate(lista):
+            nombre = item['name']
+            print(f"{idx} - {nombre}")
+    except Exception as e:
+        print(f"Error al conectarse a la base de datos: {e}")
+
+
+def program():
+    # pip install python-dotenv
+    from dotenv import load_dotenv
+    import requests
+    import influxdb
+    import os
+
+# Cargar variables de entorno desde el archivo .env
+    load_dotenv()
+
     server_ip = os.getenv('SERVER_IP')
     server_port = os.getenv('SERVER_PORT')
 
-    client = influxdb.InfluxDBClient(host=server_ip, port=server_port, database=key)
-    result = client.query("SHOW MEASUREMENTS;")
-    print("----SHOW MEASUREMENTS----")
-    lista = list(result.get_points())
-    for item in lista:
-        nombre = item['name']
-        print(f"{lista.index(item)} - {nombre}")
+    client = influxdb.InfluxDBClient(server_ip,server_port,)
+    databases = client.query("show databases;")
+
+    print("Bases de datos:")
+    #for row in resultado:
+
+    print('---------')
+    databases_set = list(databases.get_points())
+
+    # Recorrer la lista e imprimir los nombres de las bases de datos
+    for base_de_datos in databases_set:
+        nombre = base_de_datos['name']
+        print(f"{databases_set.index(base_de_datos)} - {nombre}")
 
 
-# Cargar variables de entorno desde el archivo .env
-load_dotenv()
-
-import os
-
-server_ip = os.getenv('SERVER_IP')
-server_port = os.getenv('SERVER_PORT')
-
-client = influxdb.InfluxDBClient(server_ip,server_port,)
-resultado = client.query("show databases;")
-
-print("Bases de datos:")
-#for row in resultado:
-
-print('---------')
-lista_bases_de_datos = list(resultado.get_points())
-
-# Recorrer la lista e imprimir los nombres de las bases de datos
-for base_de_datos in lista_bases_de_datos:
-    nombre = base_de_datos['name']
-    print(f"{lista_bases_de_datos.index(base_de_datos)} - {nombre}")
-
-
-key = ''
-while key != 'q':
-    try:
-        key = input("Selecciona la BBDD 'q' para salir: ")
-        index = int(key)
-        if index in range(0,len(lista_bases_de_datos)):
-            
-            #connet_bbdd(index) 
-            connet_bbdd(lista_bases_de_datos[index]['name'])   
-            key = 'q'
-    except:
-        pass
+    key = ''
+    while key != 'q':
+        try:
+            key = input("Selecciona la BBDD 'q' para salir: ")
+            index = int(key)
+            if index in range(0,len(databases_set)):
+                
+                #connet_bbdd(index) 
+                connet_bbdd(server_ip,server_port,databases_set[index]['name'],)   
+                key = 'q'
+        except:
+            pass
    
     
     
+if __name__ == "__main__":
+    requirements_check()
+    program()
